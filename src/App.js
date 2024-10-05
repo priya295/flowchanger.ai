f5d1bbede498a6314c38bb0a47e68c0d61f5898c
piyush <piyushloona@gmail.com>
2024-10-05 12:01:02 +0530

    new editstaff folder added

--- src/App.js
+++ src/App.js
@@ -13,60 +13,105 @@ import EditRole from './Admin/pages/Roles/EditRole';
 import EditDepartment from './Admin/pages/Department/EditDepartment';
 import StaffMenu from './Admin/pages/StaffSection/StaffMenu';
 import AddOneStaff from './Admin/pages/StaffSection/AddOneStaff';
+import SidebarEditStaff from './Admin/pages/editstaff/SidebarEditStaff';
+import PersonalDetail from './Admin/pages/editstaff/PersonalDetail';
+import BankDetails from '../src/Admin/pages/editstaff/EditBankDetail';
+import Editattendance from '../src/Admin/pages/editstaff/EditAttendanceDetail';
+import EmployementDetail from './Admin/pages/editstaff/EmployementDetail';
+import UserPermission from './Admin/pages/editstaff/UserPermission';
+import EditLeavePolicies from './Admin/pages/editstaff/EditLeavePolicies';
+import CustomDetail from './Admin/pages/editstaff/CustomDetail';
+import BackgroundVerification from './Admin/pages/editstaff/BackgroundVerification';
+import VerifyAadhaar from './Admin/pages/editstaff/VerifyAadhaar';
+import UpperHeader from './Admin/pages/editstaff/UpperHeader';
 
 
 const App = () => {
 
-   const [toggleSideBar , setToggleSideBar] = useState(true);
+  const [toggleSideBar, setToggleSideBar] = useState(true);
 
-   const handleToggleSideBar = () =>{
-      setToggleSideBar(!toggleSideBar)
-   }
+  const handleToggleSideBar = () => {
+    setToggleSideBar(!toggleSideBar)
+  }
+
+  function Editstaff() {
+    return (
+      <>
+        <div className=''>
+          <UpperHeader />
+          <div className='flex'>
+            <SidebarEditStaff />
+            <Outlet />
+          </div>
+        </div>
+      </>
+    )
+  }
 
   //  function to create layout of the page
-       function AdminLayout() {
-            return (
-              <>
-              <div className="flex">
-              <div className="">
-            {toggleSideBar && <SideBar toggleSideBar = {toggleSideBar}/>}
-            </div> 
-       
-         {/* container for navbar and outlet */}
-         <div className={`flex flex-col flex-grow overflow-hidden`}>
-           <NavBar handleToggleSideBar={handleToggleSideBar} toggleSideBar={toggleSideBar}/>
-           <main className={`flex-1 overflow-x-hidden overflow-y-auto  `}>
-             <div className="container mx-auto px-4 pl-3 pr-3 py-8 lg:px-4 ">
-              <Outlet />
-             </div>
-          </main>
-         </div>
-       </div>
-              </>
-            )
-          }
-          
+  function AdminLayout() {
+    return (
+      <>
+        <div className="flex">
+          <div className="">
+            {toggleSideBar && <SideBar toggleSideBar={toggleSideBar} />}
+          </div>
+
+          {/* container for navbar and outlet */}
+          <div className={`flex flex-col flex-grow overflow-hidden`}>
+            <NavBar handleToggleSideBar={handleToggleSideBar} toggleSideBar={toggleSideBar} />
+            <main className={`flex-1 overflow-x-hidden overflow-y-auto  `}>
+              <div className="container mx-auto px-4 pl-3 pr-3 py-8 lg:px-4 ">
+                <Outlet />
+              </div>
+            </main>
+          </div>
+        </div>
+      </>
+    )
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AdminLayout />}>
-          <Route path='/Project' element = {<Project/>}/>   
-          <Route path='/task' element = {<Task/>}/>   
-          
-          <Route path='/addrole' element = {<AddRole/>}/>  
-          <Route path='/role' element = {<Role/>}/>  
-          <Route path='/editrole' element = {<EditRole/>}/>  
-          <Route path='/editdepartment' element = {<EditDepartment/>}/>  
-          <Route path='/staff-menu' element = {<StaffMenu/>}/>  
-          <Route path='/add-one-staff' element = {<AddOneStaff/>}/>  
+          <Route path='/Project' element={<Project />} />
+          <Route path='/task' element={<Task />} />
+
+          <Route path='/addrole' element={<AddRole />} />
+          <Route path='/role' element={<Role />} />
+          <Route path='/editrole' element={<EditRole />} />
+          <Route path='/editdepartment' element={<EditDepartment />} />
+          <Route path='/staff-menu' element={<StaffMenu />} />
+          <Route path='/add-one-staff' element={<AddOneStaff />} />
 
 
 
-          <Route path='/department' element = {<Department/>}/>  
-          <Route path='/adddepartment' element = {<AddDepartment/>}/>  
-          
+          <Route path='/department' element={<Department />} />
+          <Route path='/adddepartment' element={<AddDepartment />} />
+
         </Route>
+
+        <Route element={<Editstaff />}>
+        <Route path='/personal-detail' element={<PersonalDetail />} />
+        <Route path='/bank-detail' element={<BankDetails />} />
+        <Route path='/attendance-detail' element={<Editattendance />} />
+        <Route path='/employee-detail' element={<EmployementDetail />} />
+        <Route path='/user-permission' element={<UserPermission />} />
+        <Route path='/leavepolicy-detail' element={<EditLeavePolicies />} />
+        <Route path='/custom-detail' element={<CustomDetail />} />
+        <Route path='/background-verification' element={<BackgroundVerification />} />
+        <Route path='/verify-aadhar' element={<VerifyAadhaar />} />
+
+
+
+
+
+      </Route>
       </Routes>
+
+    
+
     </BrowserRouter>
   );
 }
