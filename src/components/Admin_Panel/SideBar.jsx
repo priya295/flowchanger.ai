effd088d497ae46534a125756a025915379241d7
kanika <kanakarora98@gmail.com>
2024-11-11 10:44:58 +0530

    new changes

--- src/components/Admin_Panel/SideBar.jsx
+++ src/components/Admin_Panel/SideBar.jsx
@@ -66,7 +66,7 @@ const SideBar = ({ toggleSideBar }) => {
 
 
         <div className="">
-          <Link to="/projects" className={`flex items-center gap-[10px]  p-[10px] hover:bg-[#fff] rounded-md hover:text-[#8a25b0] transition-all ${selectedTab === "projects" ? "bg-white [#8a25b0]" : "text-white"}`} onClick={()=>{setSelectedTab("project")}}>
+          <Link to="/projects" className={`flex items-center gap-[10px]  p-[10px] hover:bg-[#fff] rounded-md hover:text-[#8a25b0] transition-all ${selectedTab === "project" ? "bg-white [#8a25b0] text-[#8a25b0]" : "text-white"}`} onClick={()=>{setSelectedTab("project")}}>
             {/* <img src={project} alt="" /> */}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-folder-kanban"><path d="M4 20h16a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.93a2 2 0 0 1-1.66-.9l-.82-1.2A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13c0 1.1.9 2 2 2Z"/><path d="M8 10v4"/><path d="M12 10v2"/><path d="M16 10v6"/></svg>
             <button
